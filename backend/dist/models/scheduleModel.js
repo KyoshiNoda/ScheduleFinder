1d8a761919b46ed2af91d0b7711f195a0c0e1a5b
unknown <kyoshisew@gmail.com>
2023-02-16 12:15:21 -0500

    added schedule Schema

---
+++ backend/dist/models/scheduleModel.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const timeSlotSchema = new mongoose_1.default.Schema({
+    id: { type: Number, required: true },
+    category: { type: String, required: true },
+    title: { type: String, required: true },
+    startTime: { type: String, required: true },
+    endTime: { type: String, required: true },
+    color: { type: String, required: true },
+    location: String,
+    professor: String
+});
+const daySchema = new mongoose_1.default.Schema({
+    day: { type: String, required: true },
+    timeSlot: [timeSlotSchema],
+});
+const scheduleSchema = new mongoose_1.default.Schema({
+    user: { type: String, required: true },
+    visibility: { type: String, required: true },
+    week: [daySchema],
+});
+const Schedule = mongoose_1.default.model('Schedule', scheduleSchema);
+exports.default = Schedule;
---
+++ backend/src/models/scheduleModel.ts
@@ -0,0 +1,47 @@
+import mongoose, { Schema, Document } from "mongoose";
+export interface TimeSlot {
+  id: number;
+  category: number;
+  title: string;
+  startTime: string;
+  endTime: string;
+  color: string;
+  location?: string;
+  professor?: string;
+}
+
+export interface Day {
+  day: string;
+  timeSlot: TimeSlot[];
+}
+
+export interface ISchedule extends Document {
+  user_id: string;
+  visibility: string;
+  week: Day[];
+}
+
+const timeSlotSchema: Schema = new mongoose.Schema({
+  id: { type: Number, required: true },
+  category: { type: String, required: true },
+  title: { type: String, required: true },
+  startTime: { type: String, required: true },
+  endTime: { type: String, required: true },
+  color: { type: String, required: true },
+  location: String,
+  professor: String
+});
+const daySchema: Schema = new mongoose.Schema({
+  day: { type: String, required: true },
+  timeSlot: [timeSlotSchema],
+});
+
+const scheduleSchema: Schema = new mongoose.Schema({
+  user: { type: String, required: true },
+  visibility: { type: String, required: true },
+  week: [daySchema],
+});
+
+const Schedule = mongoose.model<ISchedule>('Schedule', scheduleSchema);
+
+export default Schedule;
