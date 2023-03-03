fced2d2e06dda895a0d9dbe560b7ad5de530269b
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-03-03 15:15:57 -0500

    Adding schedule store and slice.

---
+++ client/src/redux/stores/schedulesStore.ts
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+import schedulesSlice from '../slices/schedulesSlice';
+
