8eaf39c990d6b551570d70a53911b7fe385a738c
unknown <kyoshisew@gmail.com>
2023-03-03 21:13:16 -0500

    accepted merge incoming changes on merge conflict

--- client/src/redux/stores/schedulesStore.ts
+++ client/src/redux/stores/schedulesStore.ts
@@ -2,10 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
-<<<<<<< HEAD
-import schedulesSlice from '../slices/schedulesSlice';
-
-=======
 import schedulesReducer from '../slices/SchedulesSlice';
 
 const rootReducer = combineReducers({
@@ -24,4 +20,3 @@ export default store;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
->>>>>>> d89fb339c9befacdaba6fa24e8352dde57d91bce
