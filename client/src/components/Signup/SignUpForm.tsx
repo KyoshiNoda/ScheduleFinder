70b85f36d2466b3ab6fe872c8ce6bc14080dbed2
KyoshiNoda <kyoshisew@gmail.com>
2023-03-06 15:44:17 -0500

    added signup form

--- client/src/components/Signup/SignUpForm.tsx
+++ client/src/components/Signup/SignUpForm.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 type Props = {};
 
 function SignUpForm({}: Props) {
@@ -98,9 +97,44 @@ function SignUpForm({}: Props) {
               required
             />
           </div>
+          <div>
+            <label
+              htmlFor="School"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              School
+            </label>
+            <input
+              type="text"
+              id="school"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="UCLA"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="birthdate"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Date of Birth
+            </label>
+            <input
+              type="date"
+              id="birthdate"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="UCLA"
+              required
+            />
+          </div>
         </div>
       </div>
-      <button type="button" className="text-lg w-full px-8 py-3 font-semibold rounded-full bg-gray-100 text-gray-800">Submit</button>
+      <button
+        type="button"
+        className="text-lg w-full px-8 py-3 font-semibold rounded-full bg-gray-100 text-gray-800"
+      >
+        Submit
+      </button>
     </form>
   );
 }
