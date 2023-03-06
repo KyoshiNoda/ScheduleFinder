78c6cbca704b1c40496e43c9571c7d1c2683d7d4
KyoshiNoda <kyoshisew@gmail.com>
2023-03-06 15:51:46 -0500

    refactored login with more components

--- client/src/components/Login/LoginBox.tsx
+++ client/src/components/Login/LoginBox.tsx
@@ -1,47 +1,13 @@
 import React from 'react';
-
+import LoginForm from './LoginForm';
+import LoginGoogle from './LoginGoogle';
 type Props = {};
 
 function LoginBox({ }: Props) {
   return (
     <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-100 dark:bg-slate-800 dark:text-gray-100 ">
       <h1 className="text-2xl font-bold text-center">Login</h1>
-      <form action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
-        <div className="space-y-1 text-sm">
-          <label htmlFor="email" className="block text-lg font-bold">
-            Email
-          </label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="Enter your email"
-            className="w-full px-4 py-3 bg-gray-50 rounded-md border-gray-100  dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
-          />
-        </div>
-
-        <div className="space-y-1 text-sm">
-          <label htmlFor="password" className="block text-lg font-bold">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Enter your password"
-            className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
-          />
-          <div className="flex justify-end text-xs dark:text-gray-400">
-            <a rel="noopener noreferrer" href="#">
-              Forgot Password?
-            </a>
-          </div>
-
-        </div>
-        <button className="block w-full p-3 text-center rounded-sm bg-blue-400 text-white dark:text-gray-900 font-bold dark:bg-slate-300 ">
-          Sign in
-        </button>
-      </form>
+      <LoginForm/>
       <div className="flex items-center pt-4 space-x-1">
         <div className="flex-1 h-px sm:w-16 bg-gray-900 dark:bg-gray-700"></div>
         <p className="px-3 text-sm dark:text-gray-400">
@@ -49,17 +15,7 @@ function LoginBox({ }: Props) {
         </p>
         <div className="flex-1 h-px sm:w-16 bg-gray-900 dark:bg-gray-700"></div>
       </div>
-      <div className="flex justify-center space-x-4">
-        <button aria-label="Log in with Google" className="p-3 rounded-sm">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 32 32"
-            className="w-5 h-5 fill-current"
-          >
-            <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
-          </svg>
-        </button>
-      </div>
+      <LoginGoogle/>
       <p className="text-xs text-center sm:px-6 dark:text-gray-400">
         Don't have an account? { }
         <a
