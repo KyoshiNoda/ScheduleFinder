e3a57fe52ea585a80b95e1cb8cbea8a11d0c50eb
unknown <kyoshisew@gmail.com>
2023-03-19 15:48:43 -0400

    added grid styles on personalTab

--- client/src/components/Account/Personal/PersonalTab.tsx
+++ client/src/components/Account/Personal/PersonalTab.tsx
@@ -1,76 +1,95 @@
+import { Dropdown } from "flowbite-react"
 type Props = {}
 
 function PersonalTab({ }: Props) {
   return (
     <form>
-      <div className="flex flex-col">
-        <div className="flex">
-          <div>
-            <label
-              htmlFor="first_name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              First name
-            </label>
-            <input
-              type="text"
-              id="first_name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="John"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="last_name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Last name
-            </label>
-            <input
-              type="text"
-              id="last_name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Doe"
-              required
-            />
-          </div>
+      <div className="grid grid-rows-3 grid-cols-2 gap-3">
+        <div>
+          <label
+            htmlFor="first_name"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            First name
+          </label>
+          <input
+            type="text"
+            id="first_name"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="John"
+            required
+          />
         </div>
-        <div className="flex">
-          <div>
-            <label
-              htmlFor="school"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              School
-            </label>
-            <input
-              type="text"
-              id="school"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="UCLA"
-              required
-            />
-          </div>
+        <div>
+          <label
+            htmlFor="last_name"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Last name
+          </label>
+          <input
+            type="text"
+            id="last_name"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Doe"
+            required
+          />
+        </div>
 
-          <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown button <svg className="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg></button>
-          <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-              <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-              </li>
-              <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-              </li>
-              <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-              </li>
-              <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-              </li>
-            </ul>className
-          </div>
-
+        <div>
+          <label
+            htmlFor="school"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            School
+          </label>
+          <input
+            type="text"
+            id="school"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="UCLA"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="Major"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Major
+          </label>
+          <input
+            type="text"
+            id="Major"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Computer Science"
+            required
+          />
+        </div>
+        <div>
+          <Dropdown
+            label="Gender"
+            size="lg"
+          >
+            <Dropdown.Item>
+              Male
+            </Dropdown.Item>
+            <Dropdown.Item>
+              Female
+            </Dropdown.Item>
+            <Dropdown.Item>
+              Binary
+            </Dropdown.Item>
+            <Dropdown.Item>
+              Transgender
+            </Dropdown.Item>
+            <Dropdown.Item>
+              Intersex
+            </Dropdown.Item>
+            <Dropdown.Item>
+              I prefer not to say
+            </Dropdown.Item>
+          </Dropdown>
         </div>
         <div>
           <label
@@ -83,7 +102,6 @@ function PersonalTab({ }: Props) {
             type="date"
             id="birthdate"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="UCLA"
             required
           />
         </div>
