a0cb32bef4da2904448a0ee47878ad0901bf1bdb
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-03-20 10:47:11 -0400

    Adding responsive styles to the account form.

--- client/src/components/Account/Tab.tsx
+++ client/src/components/Account/Tab.tsx
@@ -5,28 +5,34 @@ type Props = {
 
 function Tab(props: Props): JSX.Element {
   return (
-    <div className="flex justify-center text-lg font-medium text-center text-gray-500 border-gray-200 dark:text-gray-400 dark:border-gray-700">
-      <ul className="flex flex-wrap -mb-px">
-        <li className="mr-10">
+    <div className="flex justify-center text-lg mb-5 font-medium text-center text-gray-500 border-gray-200 dark:text-gray-400 dark:border-gray-700">
+      <ul className="flex flex-wrap sm:gap-5">
+        <li>
           <div
-            className={`inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 cursor-pointer ${props.activeTab === 'Profile' && 'active-tab'}`}
+            className={`inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 cursor-pointer ${
+              props.activeTab === 'Profile' && 'active-tab'
+            }`}
             aria-current="page"
             onClick={() => props.getTab('Profile')}
           >
             Profile
           </div>
         </li>
-        <li className="mr-10">
+        <li>
           <div
-            className={`inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 cursor-pointer ${props.activeTab === 'Personal' && 'active-tab'}`}
+            className={`inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 cursor-pointer ${
+              props.activeTab === 'Personal' && 'active-tab'
+            }`}
             onClick={() => props.getTab('Personal')}
           >
             Personal
           </div>
         </li>
-        <li className="mr-10">
+        <li>
           <div
-            className={`inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 cursor-pointer ${props.activeTab === 'Friends' && 'active-tab'}`}
+            className={`inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 cursor-pointer ${
+              props.activeTab === 'Friends' && 'active-tab'
+            }`}
             onClick={() => props.getTab('Friends')}
           >
             Friends
