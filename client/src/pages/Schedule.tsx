0afd5c6dd1ba58ac255b27004dce81ad66776305
KyoshiNoda <kyoshisew@gmail.com>
2023-03-21 01:47:41 -0400

    added types to dayJS

--- client/src/pages/Schedule.tsx
+++ client/src/pages/Schedule.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import Axios from 'axios';
-import dayjs from 'dayjs/esm';
+import dayjs, {Dayjs} from 'dayjs/esm';
 import customParseFormat from 'dayjs/esm/plugin/customParseFormat';
 import localizedFormat from 'dayjs/esm/plugin/localizedFormat';
 import 'dayjs/locale/en';
@@ -25,6 +25,10 @@ type Schedule = {
   visibility : string;
   timeSlot:  TimeSlot[]
 }
+type TimeSection = {
+  startTime : Dayjs;
+  endTime : Dayjs
+}
 function Schedule({ }: Props) {
   const [schedules, setSchedules] = useState<[Schedule]>();
   useEffect(() => {
@@ -33,10 +37,21 @@ function Schedule({ }: Props) {
         setSchedules(res.data);
       })
   }, [])
-  const timeSlot = schedules?.[0].timeSlot[0]; // a single timeSlot from DB
-  const startTime = dayjs(timeSlot?.startTime, 'h:mm A'); // 2023-03-21T10:25:00-04:00
-  const endTime = dayjs(timeSlot?.endTime, 'h:mm A'); // 2023-03-21T12:30:00-04:00
-  const totalTime = endTime.diff(startTime, 'hours'); // total time from timeSlot
+  const timeSlot : TimeSlot | undefined = schedules?.[0].timeSlot[0]; // a single timeSlot from DB
+  
+  const time1 : TimeSection ={ //
+    startTime : dayjs(timeSlot?.startTime, 'h:mm A'), // 2023-03-21T10:25:00-04:00
+    endTime : dayjs(timeSlot?.endTime, 'h:mm A') // 2023-03-21T12:30:00-04:00
+  };
+  const time2 : TimeSection = {
+    startTime : dayjs('10:25 AM', 'h:mm A'),
+    endTime : dayjs('12:30 PM', 'h:mm A')
+  };
+
+  let result : string = time1.startTime.isSame(time2.startTime) ? "its the same" : "not same"; // checks if start or end time are the same
+  console.log(result);
+
+  const totalTime : number = time1.endTime.diff(time1.startTime, 'hours'); // total time from timeSlot
   console.log(totalTime);
   return (
     <div>Schedule</div>
