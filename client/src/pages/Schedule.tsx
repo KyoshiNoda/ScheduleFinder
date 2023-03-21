6d9fb6e3055e0bba200e0e7f84e4023646c7bb42
unknown <kyoshisew@gmail.com>
2023-03-20 23:10:09 -0400

    getting API data

--- client/src/pages/Schedule.tsx
+++ client/src/pages/Schedule.tsx
@@ -1,9 +1,46 @@
-import * as dayjs from 'dayjs';
-import duration from 'dayjs/plugin/duration';
-type Props = {}
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import dayjs from 'dayjs/esm';
+import customParseFormat from 'dayjs/esm/plugin/customParseFormat';
+import localizedFormat from 'dayjs/esm/plugin/localizedFormat';
+import 'dayjs/locale/en';
+dayjs.extend(customParseFormat);
+dayjs.extend(localizedFormat);
+dayjs.locale('en');
 
-function Schedule({}: Props) {
-  
+type Props = {}
+type TimeSlot = {
+  day: string;
+  category: string;
+  title: string;
+  startTime: string;
+  endTime: string;
+  color: string;
+  location?: string;
+  professor?: string;
+}
+type Schedule = {
+  _id  : string;
+  user_id : string;
+  visibility : string;
+  timeSlot:  TimeSlot[]
+}
+function Schedule({ }: Props) {
+  const [schedules, setSchedules] = useState<[Schedule]>();
+  useEffect(() => {
+    Axios.get('http://localhost:3001/api/schedules')
+      .then((res) => {
+        setSchedules(res.data);
+        console.log(res.data)
+      })
+  }, [])
+  const timeBlock = schedules?.[0].timeSlot[0]
+  const startTime = dayjs(timeBlock?.startTime, 'h:mm A');
+  const endTime = dayjs(timeBlock?.endTime, 'h:mm A');
+  const totalTime = endTime.diff(startTime, 'hours');
+  console.log(timeBlock);
+  console.log(totalTime);
+
   return (
     <div>Schedule</div>
   )
