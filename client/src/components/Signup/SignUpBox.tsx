43895a228c2541cfa75555efabf544a6be4e1ab3
KyoshiNoda <kyoshisew@gmail.com>
2023-03-22 14:39:26 -0400

    added responsive layout for signup

--- client/src/components/Signup/SignUpBox.tsx
+++ client/src/components/Signup/SignUpBox.tsx
@@ -4,11 +4,11 @@ type Props = {};
 
 function SignUpBox({}: Props) {
   return (
-    <div className='flex w-1/3 flex-col bg-gray-100 dark:bg-slate-800  rounded-lg p-10 dark:text-gray-100'>
-      <div className='flex justify-center text-5xl mb-5 dark:text-white'>
+    <div className='flex w-5/6 h-full md:w-2/3 lg:w-1/3 flex-col bg-gray-100 dark:bg-slate-800  rounded-lg p-10 dark:text-gray-100'>
+      <div className='flex justify-center text-4xl md:text-5xl mb-5 dark:text-white'>
         Sign up
       </div>
-      <SignUpForm/> 
+      <SignUpForm/>
       <div className="flex items-center pt-4 space-x-1">
         <div className="flex-1 h-px sm:w-16 bg-gray-900 dark:bg-gray-700"></div>
         <p className="px-3 text-sm dark:text-gray-400">
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -12,9 +12,7 @@ function Login({}: Props) {
     const handleResize = () => {
       setWidth(window.innerWidth);
     };
-
     window.addEventListener('resize', handleResize);
-
     return () => {
       window.removeEventListener('resize', handleResize);
     };
