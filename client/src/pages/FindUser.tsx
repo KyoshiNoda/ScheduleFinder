c8c0cb7b85bbbbd2a1f14ad5b707842b1e399f19
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-03-23 11:55:56 -0400

    Created initial layout of the FindUser page.

--- client/src/pages/FindUser.tsx
+++ client/src/pages/FindUser.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import { Label, TextInput } from 'flowbite-react';
 
 const FindUser = () => {
   return (
     <div>
-      Find User
+      <div>
+        <div className="mb-2 block">
+          <Label
+            htmlFor="student-name"
+            value="Search the name of the student you are looking for!"
+          />
+        </div>
+        <TextInput
+          id="student-name"
+          type="text"
+          placeholder="Enter student name"
+          required={true}
+        />
+      </div>
     </div>
   );
 };
