47cee2d8cc54b49e4ff9a110b57099ff4e254413
KyoshiNoda <kyoshisew@gmail.com>
2023-04-03 02:46:42 -0400

    added mobile links

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -35,22 +35,34 @@ function Homepage({}: Props) {
             Easily create schedules to see your friends. <br />
             Discover new people within your free time periods.
           </p>
-          <div className="flex justify-center gap-1">
-            <button
-              type="submit"
-              form="changes"
-              className="w-1/2 items-center rounded-full bg-blue-600 px-8 py-3 font-semibold text-white"
-            >
-              Get Started
-            </button>
-            <a
-              className="w-3 cursor-pointer"
-              href={'https://github.com/KyoshiNoda/ScheduleFinder'}
-            >
-              <AiFillGithub size={'96'} color={`${theme}`} />
-            </a>
-          </div>
           <img src={scheduleImg} className="max-w-screen-sm text-center" />
+          <div className="flex justify-center gap-1">
+            <div className="self-center">
+              <a
+                className="flex h-1/2  w-full items-center justify-center rounded-full bg-blue-600  px-8 py-3 text-xl font-semibold text-white dark:bg-blue-800"
+                href={'/signup'}
+              >
+                Get Started
+              </a>
+            </div>
+
+            <div>
+              <a
+                className="w-3 cursor-pointer"
+                href={'https://github.com/KyoshiNoda/ScheduleFinder'}
+              >
+                <AiFillGithub size={'96'} color={`${theme}`} />
+              </a>
+            </div>
+            <div className="self-center">
+              <a
+                className="flex h-1/2  w-full items-center justify-center rounded-full bg-blue-600  px-8 py-3 text-xl font-semibold text-white dark:bg-blue-800"
+                href={'/login'}
+              >
+                Login
+              </a>
+            </div>
+          </div>
         </div>
       ) : (
         <div className="flex flex-col">
@@ -58,7 +70,7 @@ function Homepage({}: Props) {
             <div className="mr-1 w-1/2 flex-1 items-center">
               <img src={scheduleImg} className="inline-block w-full max-w-md" />
             </div>
-            <div className="flex flex-1 flex-col items-center justify-center text-5xl dark:text-white lg:text-7xl ml-60">
+            <div className="ml-60 flex flex-1 flex-col items-center justify-center text-5xl dark:text-white lg:text-7xl">
               <div className="my-auto">ScheduleFinder</div>
             </div>
           </div>
@@ -71,13 +83,13 @@ function Homepage({}: Props) {
             </div>
             <div className="ml-56 flex w-1/2 flex-col gap-2">
               <a
-                className="flex h-1/2 w-full items-center justify-center rounded-full bg-blue-600 dark:bg-blue-800 px-8 py-3 text-xl font-semibold text-white"
+                className="flex h-1/2 w-full items-center justify-center rounded-full bg-blue-600 px-8 py-3 text-xl font-semibold text-white dark:bg-blue-800"
                 href={'/signup'}
               >
                 Get Started
               </a>
               <a
-                className="flex h-1/2  w-full items-center justify-center rounded-full bg-blue-600  dark:bg-blue-800 px-8 py-3 text-xl font-semibold text-white"
+                className="flex h-1/2  w-full items-center justify-center rounded-full bg-blue-600  px-8 py-3 text-xl font-semibold text-white dark:bg-blue-800"
                 href={'/login'}
               >
                 Login
