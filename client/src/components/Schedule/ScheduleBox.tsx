38b171492d7be79578c90959fd5bc0599fd3d342
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-04-09 21:45:38 -0400

    Removing background color typos from the schedule columns.

--- client/src/components/Schedule/ScheduleBox.tsx
+++ client/src/components/Schedule/ScheduleBox.tsx
@@ -103,7 +103,7 @@ function ScheduleBox({}: Props) {
           <hr className="absolute top-[792px] w-full border-dotted bg-gray-400 dark:bg-gray-900" />
           <hr className="absolute top-[864px] w-full border-dotted bg-gray-400 dark:bg-gray-900" />
           <hr className="absolute top-[936px] w-full border-dotted bg-gray-400 dark:bg-gray-900" />
-          <div className="bg-purple-5 relative h-[1008px] w-1/5">
+          <div className="relative h-[1008px] w-1/5">
             <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">
               monday
             </h2>
@@ -128,7 +128,7 @@ function ScheduleBox({}: Props) {
                   />
                 ))}
           </div>
-          <div className="bg-red-5 relative h-[1008px] w-1/5">
+          <div className="relative h-[1008px] w-1/5">
             <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">
               tuesday
             </h2>
@@ -153,7 +153,7 @@ function ScheduleBox({}: Props) {
                   />
                 ))}
           </div>
-          <div className="bg-green-5 relative h-[1008px] w-1/5">
+          <div className="relative h-[1008px] w-1/5">
             <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">
               wednesday
             </h2>
@@ -178,7 +178,7 @@ function ScheduleBox({}: Props) {
                   />
                 ))}
           </div>
-          <div className="bg-blue-5 relative h-[1008px] w-1/5">
+          <div className="relative h-[1008px] w-1/5">
             <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">
               thursday
             </h2>
@@ -203,7 +203,7 @@ function ScheduleBox({}: Props) {
                   />
                 ))}
           </div>
-          <div className="bg-teal-5 relative h-[1008px] w-1/5">
+          <div className="relative h-[1008px] w-1/5">
             <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">
               friday
             </h2>
