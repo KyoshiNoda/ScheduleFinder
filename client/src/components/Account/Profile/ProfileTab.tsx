7e3269a225f9b2ad1faab2faf1127a4e693cc2c7
KyoshiNoda <kyoshisew@gmail.com>
2023-04-13 12:18:04 -0400

    added profile tab with correct user JWT

--- client/src/components/Account/Profile/ProfileTab.tsx
+++ client/src/components/Account/Profile/ProfileTab.tsx
@@ -1,25 +1,14 @@
 import ProfilePic from './ProfilePic';
+import { useAppSelector } from '../../../redux/store';
 type Props = {};
 
 function ProfileTab({}: Props) {
+  const {userInfo} = useAppSelector((state) => state.auth);
   return (
     <div>
       <div className="flex items-center gap-3 py-4">
-        <ProfilePic />
+        <ProfilePic picture = {userInfo.photoURL}/>
         <div className="dark:text-white text-2xl font-md">Kyoshi Noda</div>
-      </div>
-      <div className="mb-6">
-        <label
-          htmlFor="default-input"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Username
-        </label>
-        <input
-          type="text"
-          id="default-input"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        />
       </div>
       <div className="mb-6">
         <label
@@ -32,6 +21,7 @@ function ProfileTab({}: Props) {
           type="text"
           id="default-input"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder= {userInfo.email}
         />
       </div>
       <div className="flex flex-col gap-3">
