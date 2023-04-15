98db56e61196515d4b1bf7e0344a5732a52d2c86
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-04-15 13:04:28 -0400

    Implemented the mergeSchedules methods.

--- client/src/pages/CompareSchedule.tsx
+++ client/src/pages/CompareSchedule.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { useGetScheduleQuery } from '../redux/services/auth/authService';
 import { Button } from 'flowbite-react';
+import dayjs from 'dayjs';
 
 type days = {
   monday: boolean;
@@ -43,25 +44,40 @@ const CompareSchedule = () => {
     pollingInterval: 900000,
   });
 
+  // This schedule is used to initialize the scheduleB state with an initial schedule and avoid
+  // annoying errors about it potentially being undefined.
+  const defaultSchedule = {
+    _id: '',
+    user_id: '',
+    visibility: '',
+    timeSlot: [],
+  };
+
   // Schedule B is the schedule to compare against.
-  const [scheduleB, setScheduleB] = useState<Schedule>();
+  const [scheduleB, setScheduleB] = useState<Schedule>(defaultSchedule);
 
   useEffect(() => {
     fetch(`http://localhost:3001/api/schedules/${userId}/user`)
-      .then((res) => res.json()) 
+      .then((res) => res.json())
       .then((data) => {
-        setScheduleB(data[0]); 
+        setScheduleB(data[0]);
         setTimeSlots(data[0].timeSlot);
       })
       .catch((err) => console.log(err));
   }, []);
+
+  const mergeSchedules = () => {
+    const mergedSchedule = [...data[0].timeSlot, ...scheduleB.timeSlot];
+    console.log(mergedSchedule);
+    return mergedSchedule;
+  };
 
   return (
     <div className="min-h-full bg-slate-400 p-6 dark:bg-slate-900">
       <div className="flex flex-col items-center gap-16">
         <Button.Group outline={true}>
           <Button
-            onClick={() => setTimeSlots(scheduleB?.timeSlot)}
+            onClick={() => setTimeSlots(scheduleB.timeSlot)}
             color="gray"
           >
             Other user schedule
@@ -70,9 +86,7 @@ const CompareSchedule = () => {
             My schedule
           </Button>
           <Button
-            onClick={() =>
-              setTimeSlots([...data[0].timeSlot].concat(scheduleB?.timeSlot))
-            }
+            onClick={() => setTimeSlots(mergeSchedules)}
             color="gray"
           >
             Compare schedules
