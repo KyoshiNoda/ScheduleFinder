9ccfbb21460ac42d6d1ef6c0add6faf337fdbf60
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-04-27 00:46:59 -0400

    Finished initial styling in the smallest screen size.

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Toggle from '../components/Toggle';
 import scheduleImg from '../assets/schedule.png';
 import { AiFillGithub } from 'react-icons/ai';
@@ -6,86 +6,49 @@ import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
   const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-  const [width, setWidth] = useState<number>(window.innerWidth);
+
   const [theme, setTheme] = useState<string>(
     mediaQuery.matches ? 'white' : 'black'
   );
-  useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-    };
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+
   return (
     <div className="flex min-h-full w-full flex-col gap-6 bg-slate-100 p-3 dark:bg-slate-900">
       <div className="flex justify-end">
         <Toggle getTheme={(theme) => setTheme(theme)} />
       </div>
-      {width < 640 ? (
-        <div className="slate-50 flex flex-col gap-4">
-          <h1 className="flex justify-center text-4xl dark:text-white">
-            Schedule Finder
-          </h1>
-          <p className="flex justify-center px-1 text-sm font-medium dark:text-white">
-            Easily create schedules to see your friends. Discover new people
-            within your free time periods.
-          </p>
-          <div className="my-6 flex flex-col gap-6">
-            <img src={scheduleImg} className="max-w-screen-sm text-center" />
-            <div className="space-y-3">
-              <Link
-                className="flex h-1/2  w-full items-center justify-center rounded-full bg-blue-600  px-8 py-3 text-xl font-semibold text-white dark:bg-blue-800"
-                to={'/signup'}
-              >
-                Get Started
-              </Link>
-              <Link
-                className="flex h-1/2  w-full items-center justify-center rounded-full bg-blue-600  px-8 py-3 text-xl font-semibold text-white dark:bg-blue-800"
-                to={'/login'}
-              >
-                Login
-              </Link>
-            </div>
+      <div className="slate-50 flex flex-col gap-4">
+        <h1 className="flex justify-center text-4xl dark:text-white">
+          Schedule Finder
+        </h1>
+        <p className="flex justify-center px-1 text-center text-sm font-medium dark:text-white">
+          Easily create schedules to see your friends. Discover new people
+          within your free time periods.
+        </p>
+        <div className="my-6 flex flex-col gap-6">
+          <img src={scheduleImg} className="max-w-screen-sm text-center" />
+          <div className="space-y-3">
+            <Link
+              className="flex h-1/2  w-full items-center justify-center rounded-full bg-blue-600  px-8 py-3 text-xl font-semibold text-white dark:bg-blue-800"
+              to={'/signup'}
+            >
+              Get Started
+            </Link>
+            <Link
+              className="flex h-1/2  w-full items-center justify-center rounded-full bg-blue-600  px-8 py-3 text-xl font-semibold text-white dark:bg-blue-800"
+              to={'/login'}
+            >
+              Login
+            </Link>
           </div>
-          <a
-            className="w-3 cursor-pointer"
-            href="https://github.com/KyoshiNoda/ScheduleFinder"
-            target="_blank"
-          >
-            <AiFillGithub size={'50'} color={`${theme}`} />
-          </a>
         </div>
-      ) : (
-        <div className="flex flex-col">
-          <div className="flex items-center p-7">
-            <div className="mr-1 w-1/2 flex-1 items-center">
-              <img
-                src={scheduleImg}
-                alt="Logo of Schedule Finder"
-                className="inline-block w-full max-w-md"
-              />
-            </div>
-          </div>
-          <div className="flex p-3">
-            <div className="inline-block w-1/2">
-              <p className="text-2xl font-medium  dark:text-white">
-                Easily create schedules to see your friends. <br />
-                Discover new people within your free time periods.
-              </p>
-            </div>
-          </div>
-          <a
-            className="w-3 cursor-pointer"
-            href={'https://github.com/KyoshiNoda/ScheduleFinder'}
-            target="_blank"
-          >
-            <AiFillGithub size={'96'} color={`${theme}`} />
-          </a>
-        </div>
-      )}
+        <a
+          className="w-3 cursor-pointer"
+          href="https://github.com/KyoshiNoda/ScheduleFinder"
+          target="_blank"
+        >
+          <AiFillGithub size={'50'} color={`${theme}`} />
+        </a>
+      </div>
     </div>
   );
 };
