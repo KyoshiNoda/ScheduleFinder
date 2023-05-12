7f8c45407d6bef59cacb430090e034f9d72e7eee
KyoshiNoda <kyoshisew@gmail.com>
2023-05-12 18:25:01 -0400

    added Date placeholder with prior users DOB

--- client/src/components/Account/Personal/PersonalTab.tsx
+++ client/src/components/Account/Personal/PersonalTab.tsx
@@ -175,6 +175,7 @@ function PersonalTab() {
                   type="date"
                   id="birthdate"
                   className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                  defaultValue={userInfo.birthday.toString().substring(0, 10)}
                   required
                 />
               </div>
