bc532347252ebe1891c6a2f0731bf92e95584a78
Kyoshi Noda <62672803+KyoshiNoda@users.noreply.github.com>
2023-08-05 01:12:28 -0400

    Update environment.ts

--- client/src/utils/environment.ts
+++ client/src/utils/environment.ts
@@ -1,10 +1,10 @@
 export const getApiUrl = (): string => {
   // When merging, change this to the current environment
-  let environment = 'local';
+  let environment = 'dev';
 
   let apiUrl = '';
 
-  if (environment === 'dev') {
+  if (environment === 'local') {
     apiUrl = 'http://localhost:3001/';
   } else if (environment === 'dev') {
     apiUrl = 'https://schedulefinder-development.up.railway.app/';
