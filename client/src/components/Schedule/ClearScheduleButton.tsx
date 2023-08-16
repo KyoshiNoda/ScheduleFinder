17562ff87165045b1f180fd936c936cb4a5761f1
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-08-16 01:14:39 -0500

    Added Redux Toolkit endpoint to clear schedule and implemented frontend logic to send request.

--- client/src/components/Schedule/ClearScheduleButton.tsx
+++ client/src/components/Schedule/ClearScheduleButton.tsx
@@ -1,8 +1,15 @@
 import { useState } from 'react';
-import { Modal, Button } from 'flowbite-react';
+import { useClearScheduleMutation } from '../../redux/services/schedule/scheduleService';
+import { Modal } from 'flowbite-react';
 
-function ClearScheduleButton() {
+type Props = {
+  scheduleId: string;
+};
+
+function ClearScheduleButton({ scheduleId }: Props) {
   const [openModal, setOpenModal] = useState<boolean>(false);
+
+  const [clearSchedule] = useClearScheduleMutation();
 
   return (
     <>
@@ -13,13 +20,21 @@ function ClearScheduleButton() {
       >
         clear schedule
       </button>
-      <Modal className="pt-20" show={openModal} onClose={() => setOpenModal(false)}>
+      <Modal
+        className="pt-20"
+        show={openModal}
+        onClose={() => setOpenModal(false)}
+      >
         <Modal.Body className="py-20">
           <p className="text-center text-2xl text-white">
             Are you sure you want to clear your schedule?
           </p>
           <div className="mt-5 flex justify-center">
             <button
+              onClick={() => {
+                clearSchedule({ scheduleId });
+                setOpenModal(false)
+              }}
               type="button"
               className="mr-2 mb-2 rounded-lg bg-green-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
             >
