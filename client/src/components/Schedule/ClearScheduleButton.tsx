0315e29e0fd2b51e57f9a59ac4a5a202f976f464
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-08-15 23:58:02 -0500

    Adding clear schedule button to the UI.

---
+++ client/src/components/Schedule/ClearScheduleButton.tsx
@@ -0,0 +1,12 @@
+function ClearScheduleButton() {
+  return (
+    <button
+      type="button"
+      className="mr-2 mb-2 rounded-lg border border-red-700 px-6 py-3.5 text-center text-sm font-medium text-red-700 hover:bg-red-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-red-300 dark:border-red-500 dark:text-red-500 dark:hover:bg-red-600 dark:hover:text-white dark:focus:ring-red-900"
+    >
+      CLEAR SCHEDULE
+    </button>
+  );
+}
+
+export default ClearScheduleButton;
