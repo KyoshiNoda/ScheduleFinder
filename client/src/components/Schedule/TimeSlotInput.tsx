40acc1caa183f4623af5a1e523eff0b8c09c0876
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-08-31 16:54:14 -0400

    Removing unncessary spaces that got added when pasting the code.

--- client/src/components/Schedule/TimeSlotInput.tsx
+++ client/src/components/Schedule/TimeSlotInput.tsx
@@ -55,7 +55,9 @@ const TimeSlotInput = () => {
     scheduleID = data[0]._id;
   }
   const addTimeSlot = async (event: React.FormEvent<HTMLFormElement>) => {
-    event?.preventDefault(); // If no checkboxes have been selected, the form shouldn't be submitted.
+    event?.preventDefault(); 
+
+    // If no checkboxes have been selected, the form shouldn't be submitted.
     if (
       !(
         mondayRef.current.checked ||
@@ -146,24 +148,18 @@ const TimeSlotInput = () => {
   };
   return (
     <>
-      {' '}
       {!isFetching && data && (
         <div className="flex flex-col">
-          {' '}
-          <ClearScheduleButton scheduleId={scheduleID} />{' '}
+          <ClearScheduleButton scheduleId={scheduleID} />
           <div className="mt-6 flex  flex-col rounded-lg bg-slate-50 p-5 dark:bg-black sm:h-1/2">
-            {' '}
             <form ref={formRef} onSubmit={addTimeSlot} className="space-y-2">
-              {' '}
               <div>
-                {' '}
                 <label
                   htmlFor="title"
                   className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  {' '}
-                  Title{' '}
-                </label>{' '}
+                  Title
+                </label>
                 <input
                   ref={titleRef}
                   required
@@ -171,146 +167,122 @@ const TimeSlotInput = () => {
                   id="title"
                   className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-center text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                   placeholder="BIO 130"
-                />{' '}
-              </div>{' '}
+                />
+              </div>
               <div className="mt-2">
-                {' '}
                 <label
                   htmlFor="days"
                   className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  {' '}
-                  Days{' '}
-                </label>{' '}
+                  Days
+                </label>
                 <ul
                   className={`w-full items-center rounded-lg border bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white sm:flex ${
                     daysError ? ' border-rose-400 dark:border-rose-400' : ''
                   }`}
                 >
-                  {' '}
                   <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 sm:border-r">
-                    {' '}
                     <div className="flex items-center pl-3">
-                      {' '}
                       <input
                         ref={mondayRef}
                         id="monday"
                         type="checkbox"
                         value="monday"
                         className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
-                      />{' '}
+                      />
                       <label
                         htmlFor="monday"
                         className="ml-2 w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
                       >
-                        {' '}
-                        Mon{' '}
-                      </label>{' '}
-                    </div>{' '}
-                  </li>{' '}
+                        Mon
+                      </label>
+                    </div>
+                  </li>
                   <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 sm:border-r">
-                    {' '}
                     <div className="flex items-center pl-3">
-                      {' '}
                       <input
                         ref={tuesdayRef}
                         id="tuesday"
                         type="checkbox"
                         value="tuesday"
                         className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
-                      />{' '}
+                      />
                       <label
                         htmlFor="tuesday"
                         className="ml-2 w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
                       >
-                        {' '}
-                        Tues{' '}
-                      </label>{' '}
-                    </div>{' '}
-                  </li>{' '}
+                        Tues
+                      </label>
+                    </div>
+                  </li>
                   <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 sm:border-r">
-                    {' '}
                     <div className="flex items-center pl-3">
-                      {' '}
                       <input
                         ref={wednesdayRef}
                         id="wednesday"
                         type="checkbox"
                         value="wednesday"
                         className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
-                      />{' '}
+                      />
                       <label
                         htmlFor="wednesday"
                         className="ml-2 w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
                       >
-                        {' '}
-                        Wed{' '}
-                      </label>{' '}
-                    </div>{' '}
-                  </li>{' '}
+                        Wed
+                      </label>
+                    </div>
+                  </li>
                   <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 sm:border-r">
-                    {' '}
                     <div className="flex items-center pl-3">
-                      {' '}
                       <input
                         ref={thursdayRef}
                         id="thursday"
                         type="checkbox"
                         value="thursday"
                         className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
-                      />{' '}
+                      />
                       <label
                         htmlFor="thursday"
                         className="ml-2 w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
                       >
-                        {' '}
-                        Thurs{' '}
-                      </label>{' '}
-                    </div>{' '}
-                  </li>{' '}
+                        Thurs
+                      </label>
+                    </div>
+                  </li>
                   <li className="w-full dark:border-gray-600">
-                    {' '}
                     <div className="flex items-center pl-3">
-                      {' '}
                       <input
                         ref={fridayRef}
                         id="friday"
                         type="checkbox"
                         value="friday"
                         className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
-                      />{' '}
+                      />
                       <label
                         htmlFor="friday"
                         className="ml-2 w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
                       >
-                        {' '}
-                        Fri{' '}
-                      </label>{' '}
-                    </div>{' '}
-                  </li>{' '}
-                </ul>{' '}
+                        Fri
+                      </label>
+                    </div>
+                  </li>
+                </ul>
                 <div className="flex w-full justify-center">
-                  {' '}
                   {daysError && (
                     <p className="text-rose-500">Please pick a day!</p>
-                  )}{' '}
-                </div>{' '}
-              </div>{' '}
+                  )}
+                </div>
+              </div>
               <div className="flex flex-col">
-                {' '}
                 <div>
-                  {' '}
                   <label
                     htmlFor="startTime"
                     className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    {' '}
-                    Start Time{' '}
-                  </label>{' '}
+                    Start Time
+                  </label>
                   <div className="flex items-center">
-                    {' '}
                     <div className="space-x-2">
-                      {' '}
                       <input
                         ref={startTimeHourRef}
                         type="text"
@@ -320,8 +292,8 @@ const TimeSlotInput = () => {
                         }`}
                         placeholder="12"
                         required
-                      />{' '}
-                      <span className="text-black dark:text-white">:</span>{' '}
+                      />
+                      <span className="text-black dark:text-white">:</span>
                       <input
                         ref={startTimeMinutesRef}
                         type="text"
@@ -331,27 +303,22 @@ const TimeSlotInput = () => {
                         }`}
                         placeholder="00"
                         required
-                      />{' '}
-                    </div>{' '}
+                      />
+                    </div>
                     <Select id="startMeridiemTime" className="w-1/3" required>
-                      {' '}
-                      <option>AM</option> <option>PM</option>{' '}
-                    </Select>{' '}
-                  </div>{' '}
-                </div>{' '}
+                      <option>AM</option> <option>PM</option>
+                    </Select>
+                  </div>
+                </div>
                 <div>
-                  {' '}
                   <label
                     htmlFor="startTime"
                     className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    {' '}
-                    End Time{' '}
-                  </label>{' '}
+                    End Time
+                  </label>
                   <div className="flex items-center">
-                    {' '}
                     <div className="space-x-2">
-                      {' '}
                       <input
                         ref={endTimeHourRef}
                         type="text"
@@ -361,8 +328,8 @@ const TimeSlotInput = () => {
                         }`}
                         placeholder="12"
                         required
-                      />{' '}
-                      <span className="text-black dark:text-white">:</span>{' '}
+                      />
+                      <span className="text-black dark:text-white">:</span>
                       <input
                         ref={endTimeMinutesRef}
                         type="text"
@@ -372,58 +339,49 @@ const TimeSlotInput = () => {
                         }`}
                         placeholder="00"
                         required
-                      />{' '}
-                    </div>{' '}
+                      />
+                    </div>
                     <Select id="startMeridiemTime" className="w-1/3" required>
-                      {' '}
-                      <option>AM</option> <option>PM</option>{' '}
-                    </Select>{' '}
-                  </div>{' '}
-                </div>{' '}
-              </div>{' '}
+                      <option>AM</option> <option>PM</option>
+                    </Select>
+                  </div>
+                </div>
+              </div>
               <div className="flex gap-12">
-                {' '}
                 <div className="w-full">
-                  {' '}
                   <label
                     htmlFor="location"
                     className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    {' '}
-                    Location{' '}
-                  </label>{' '}
+                    Location
+                  </label>
                   <input
                     ref={locationRef}
                     type="text"
                     id="location"
                     className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-center text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                     placeholder="Whitman Hall"
-                  />{' '}
-                </div>{' '}
+                  />
+                </div>
                 <div className="w-1/2">
-                  {' '}
                   <label
                     htmlFor="professor"
                     className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    {' '}
-                    Professor{' '}
-                  </label>{' '}
+                    Professor
+                  </label>
                   <input
                     ref={professorRef}
                     type="text"
                     id="professor"
                     className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-center text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                     placeholder="Dr. Gerstl"
-                  />{' '}
-                </div>{' '}
-              </div>{' '}
+                  />
+                </div>
+              </div>
               <div>
-                {' '}
                 <div className="flex justify-center">
-                  {' '}
                   <div className="my-2 grid grid-cols-7 gap-2">
-                    {' '}
                     {colors.map((color) => (
                       <div
                         onClick={() => setTimeSlotColor(color)}
@@ -434,24 +392,22 @@ const TimeSlotInput = () => {
                             : 'border-none'
                         }`}
                       />
-                    ))}{' '}
-                  </div>{' '}
-                </div>{' '}
+                    ))}
+                  </div>
+                </div>
                 {colorError && (
                   <p className="text-center font-bold text-rose-500">
-                    {' '}
-                    Please pick a color!{' '}
+                    Please pick a color!
                   </p>
-                )}{' '}
-              </div>{' '}
+                )}
+              </div>
               <button
                 type="submit"
                 className="w-full rounded-full bg-blue-400 px-8 py-3 text-lg font-semibold text-white hover:bg-blue-600 dark:bg-slate-300 dark:text-black hover:dark:bg-slate-400"
               >
-                {' '}
-                Submit{' '}
-              </button>{' '}
-            </form>{' '}
+                Submit
+              </button>
+            </form>
             {timeSlotError && (
               <Modal
                 show={timeSlotError}
@@ -464,19 +420,14 @@ const TimeSlotInput = () => {
                   formRef.current.reset();
                 }}
               >
-                {' '}
-                <Modal.Header />{' '}
+                <Modal.Header />
                 <Modal.Body>
-                  {' '}
                   <div className="text-center">
-                    {' '}
-                    <AiFillWarning className="mx-auto mb-4 h-14 w-14 text-red-400 dark:text-gray-200" />{' '}
+                    <AiFillWarning className="mx-auto mb-4 h-14 w-14 text-red-400 dark:text-gray-200" />
                     <h3 className="mb-5 text-lg font-normal text-red-500 dark:text-gray-400">
-                      {' '}
-                      There is an existing TimeSlot!{' '}
-                    </h3>{' '}
+                      There is an existing TimeSlot!
+                    </h3>
                     <div className="flex justify-center gap-4">
-                      {' '}
                       <Button
                         color="gray"
                         onClick={() => {
@@ -486,17 +437,16 @@ const TimeSlotInput = () => {
                           formRef.current.reset();
                         }}
                       >
-                        {' '}
-                        Ok Thank you!{' '}
-                      </Button>{' '}
-                    </div>{' '}
-                  </div>{' '}
-                </Modal.Body>{' '}
+                        Ok Thank you!
+                      </Button>
+                    </div>
+                  </div>
+                </Modal.Body>
               </Modal>
-            )}{' '}
-          </div>{' '}
+            )}
+          </div>
         </div>
-      )}{' '}
+      )}
     </>
   );
 };
