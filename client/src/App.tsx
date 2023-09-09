a7374f044e5c6c93fda137bc250839b27a605a33
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-09-09 15:06:01 -0400

    Removing toggle component from these files.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Signup from './pages/Signup';
-import Homepage from './pages/LandingPage';
+import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import Account from './pages/Account';
 import Schedule from './pages/Schedule';
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgotPassword" element={<ForgotPassword />} />
