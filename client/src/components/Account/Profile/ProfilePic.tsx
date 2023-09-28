7e2016e8218164e8f8547fa4e3d3d931c354b948
KyoshiNoda <kyoshisew@gmail.com>
2023-09-28 16:58:57 -0400

    added styles on editmode and fixed defaultValue on email

--- client/src/components/Account/Profile/ProfilePic.tsx
+++ client/src/components/Account/Profile/ProfilePic.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
+import { AiTwotoneEdit } from 'react-icons/ai';
 type Props = {
   picture: string | undefined;
 };
 
 const ProfilePic = (props: Props) => {
+  const [isHovering, setIsHovering] = useState<boolean>(false);
   return (
-    <img
-      alt=""
-      className="h-24 w-24 rounded-full border dark:border-gray-700 dark:bg-gray-500"
-      src={props.picture}
-    />
+    <div
+      className="relative cursor-pointer"
+      onMouseEnter={() => {
+        setIsHovering(true);
+      }}
+      onMouseLeave={() => setIsHovering(false)}
+    >
+      {isHovering && (
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-75 rounded-full">
+          <AiTwotoneEdit className="rounded-full  p-1 text-2xl text-white" size={40}/>
+        </div>
+      )}
+      <img alt="" className="h-24 w-24 rounded-full border dark:border-gray-700 dark:bg-gray-500" src={props.picture} />
+    </div>
   );
 };
 
