221f1aacb63b5f2a32342ff8a52bda7543b276e3
kyoshinoda <kyoshisew@gmail.com>
2024-01-08 17:51:23 -0500

    refactored UserContainer

--- client/src/components/findUsers/User.tsx
+++ client/src/components/findUsers/User.tsx
@@ -1,5 +1,5 @@
 import { Card } from 'flowbite-react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {
   useSendFriendRequestMutation,
   useAcceptFriendRequestMutation,
@@ -9,25 +9,21 @@ import { BiTime } from 'react-icons/bi';
 import { useState } from 'react';
 import { useToast } from '../../utils/functions';
 import { ToastEnum } from '../../enums';
+import { User as UserType } from '../../types';
+
 type UserProps = {
-  id: string;
-  photoURL: string;
-  firstName: string;
-  lastName: string;
-  school: string;
-  major: string | undefined;
+  user: UserType;
   isPending: boolean;
   isFriendRequest: boolean;
   isFriends: boolean;
 };
-
-const User = ({ id, photoURL, firstName, lastName, school, major, isPending, isFriendRequest, isFriends }: UserProps) => {
+const User = ({ user, isPending, isFriendRequest, isFriends }: UserProps) => {
   const { showToast } = useToast();
   const [sendFriendRequest] = useSendFriendRequestMutation();
   const [acceptFriendRequest] = useAcceptFriendRequestMutation();
   const [removeSendFriendRequest] = useRemovePendingFriendRequestMutation();
   const [isHovered, setIsHovered] = useState<boolean>(false);
-  const fullName = `${firstName} ${lastName}`;
+  const fullName = `${user.firstName} ${user.lastName}`;
 
   const sendFriendRequestHandler = async (id: string) => {
     await sendFriendRequest({ friendID: id });
@@ -45,18 +41,19 @@ const User = ({ id, photoURL, firstName, lastName, school, major, isPending, isF
   };
 
   return (
-    <Link className="max-w-sm cursor-pointer" to={`/auth/user/${id}`}>
+    <>
       <Card>
         <div className="flex flex-col items-center">
-          <img className="mb-3 max-h-32 w-32 rounded-full object-cover shadow-lg" src={photoURL} alt={`Profile picture of ${fullName}`} />
+          <img className="mb-3 max-h-32 w-32 rounded-full object-cover shadow-lg" src={user.photoURL} alt={`Profile picture of ${fullName}`} />
           <h2 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{fullName}</h2>
-          <span className="text-sm text-gray-500 dark:text-gray-400">{major}</span>
-          <span className="text-sm text-gray-500 dark:text-gray-400">{school}</span>
+          <span className="text-sm text-gray-500 dark:text-gray-400">{user.major}</span>
+          <span className="text-sm text-gray-500 dark:text-gray-400">{user.school}</span>
+          
           <div className="mt-4 flex space-x-3 lg:mt-6">
             {!isPending ? (
               isFriendRequest ? (
                 <button
-                  onClick={() => acceptFriendRequestHandler(id)}
+                  onClick={() => acceptFriendRequestHandler(user._id)}
                   className="inline-flex items-center rounded-lg bg-green-600 px-4 py-2 text-center text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800"
                 >
                   Accept Friend Request
@@ -65,7 +62,7 @@ const User = ({ id, photoURL, firstName, lastName, school, major, isPending, isF
                 <>
                   {!isFriends && (
                     <button
-                      onClick={() => sendFriendRequestHandler(id)}
+                      onClick={() => sendFriendRequestHandler(user._id)}
                       className="inline-flex items-center rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
                       Add Friend
@@ -77,7 +74,7 @@ const User = ({ id, photoURL, firstName, lastName, school, major, isPending, isF
               <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
                 {isHovered ? (
                   <button
-                    onClick={() => cancelFriendRequestHandler(id)}
+                    onClick={() => cancelFriendRequestHandler(user._id)}
                     className="inline-flex items-center rounded-lg bg-red-600 px-1 py-2 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-red-300 dark:focus:ring-red-800"
                   >
                     Cancel Request
@@ -90,7 +87,7 @@ const User = ({ id, photoURL, firstName, lastName, school, major, isPending, isF
               </div>
             )}
             <Link
-              to={`/auth/compareSchedule/${id}`}
+              to={`/auth/compareSchedule/${user._id}`}
               className="inline-flex items-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
             >
               See schedule
@@ -98,7 +95,7 @@ const User = ({ id, photoURL, firstName, lastName, school, major, isPending, isF
           </div>
         </div>
       </Card>
-    </Link>
+    </>
   );
 };
 
