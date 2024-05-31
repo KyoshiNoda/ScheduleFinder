2b013f6dd46eac437d1877a50aeb3fdf524cd5e5
kyoshinoda <kyoshisew@gmail.com>
2024-05-31 09:59:27 -0400

    added color

--- client/src/pages/Friends.tsx
+++ client/src/pages/Friends.tsx
@@ -38,7 +38,7 @@ const Friends = () => {
         <Modal.Header />
         <Modal.Body>
           <div className="text-center">
-            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-white dark:text-gray-200" />
+            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
             <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to remove this person?</h3>
             <div className="flex justify-center gap-4">
               <Button color="failure" onClick={() => onConfirmDeleteFriend()}>
