33351100d0162badf2315b257f0726775540869c
kyoshinoda <kyoshisew@gmail.com>
2024-06-08 18:04:01 -0400

    wip

--- client/src/components/Schedule/ScheduleBox.tsx
+++ client/src/components/Schedule/ScheduleBox.tsx
@@ -16,21 +16,32 @@ const ScheduleBox = ({ timeSlots }: Props) => {
   return (
     <>
       <div className="flex w-full flex-col border shadow dark:border-none dark:shadow-none">
-        <div className="relative mb-6 flex h-[1008px] rounded bg-white dark:bg-black dark:text-white">
-          <span className="absolute -left-5 top-[-11px] font-bold">7</span>
-          <span className="absolute -left-5 top-[61px] font-bold">8</span>
-          <span className="absolute -left-5 top-[133px] font-bold">9</span>
-          <span className="absolute -left-6 top-[205px] font-bold">10</span>
-          <span className="absolute -left-6 top-[277px] font-bold">11</span>
-          <span className="absolute -left-6 top-[349px] font-bold">12</span>
-          <span className="absolute -left-5 top-[421px] font-bold">1</span>
-          <span className="absolute -left-5 top-[493px] font-bold">2</span>
-          <span className="absolute -left-5 top-[565px] font-bold">3</span>
-          <span className="absolute -left-5 top-[637px] font-bold">4</span>
-          <span className="absolute -left-5 top-[709px] font-bold">5</span>
-          <span className="absolute -left-5 top-[781px] font-bold">6</span>
-          <span className="absolute -left-5 top-[853px] font-bold">7</span>
-          <span className="absolute -left-5 top-[925px] font-bold">8</span>
+        <div className="relative mb-6 flex h-[678px] rounded overflow-scroll bg-white dark:bg-black dark:text-white">
+          <span className="absolute left-1 top-[20px] font-bold">12 AM</span>
+          <span className="absolute left-1 top-[92px] font-bold">1 AM</span>
+          <span className="absolute left-1 top-[164px] font-bold">2 AM</span>
+          <span className="absolute left-1 top-[236px] font-bold">3 AM</span>
+          <span className="absolute left-1 top-[308px] font-bold">4 AM</span>
+          <span className="absolute left-1 top-[380px] font-bold">5 AM</span>
+          <span className="absolute left-1 top-[452px] font-bold">6 AM</span>
+          <span className="absolute left-1 top-[524px] font-bold">7 AM</span>
+          <span className="absolute left-1 top-[596px] font-bold">8 AM</span>
+          <span className="absolute left-1 top-[668px] font-bold">9 AM</span>
+          <span className="absolute left-1 top-[740px] font-bold">10 AM</span>
+          <span className="absolute left-1 top-[812px] font-bold">11 AM</span>
+          <span className="absolute left-1 top-[884px] font-bold">12 PM</span>
+          <span className="absolute left-1 top-[956px] font-bold">1 PM</span>
+          <span className="absolute left-1 top-[1028px] font-bold">2 PM</span>
+          <span className="absolute left-1 top-[1100px] font-bold">3 PM</span>
+          <span className="absolute left-1 top-[1172px] font-bold">4 PM</span>
+          <span className="absolute left-1 top-[1244px] font-bold">5 PM</span>
+          <span className="absolute left-1 top-[1316px] font-bold">6 PM</span>
+          <span className="absolute left-1 top-[1388px] font-bold">7 PM</span>
+          <span className="absolute left-1 top-[1460px] font-bold">8 PM</span>
+          <span className="absolute left-1 top-[1532px] font-bold">9 PM</span>
+          <span className="absolute left-1 top-[1604px] font-bold">10 PM</span>
+          <span className="absolute left-1 top-[1676px] font-bold">11 PM</span>
+
           <hr className="absolute top-0 w-full border-dotted bg-gray-400 dark:bg-gray-900" />
           <hr className="absolute top-[72px] w-full border-dotted bg-gray-400 dark:bg-gray-900" />
           <hr className="absolute top-[144px] w-full border-dotted bg-gray-400 dark:bg-gray-900" />
@@ -46,7 +57,9 @@ const ScheduleBox = ({ timeSlots }: Props) => {
           <hr className="absolute top-[864px] w-full border-dotted bg-gray-400 dark:bg-gray-900" />
           <hr className="absolute top-[936px] w-full border-dotted bg-gray-400 dark:bg-gray-900" />
           <div className="relative mx-2 h-[1008px] w-1/5">
-            <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">{window.innerWidth < 535 ? 'm' : 'monday'}</h2>
+            <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">
+              {window.innerWidth < 535 ? 'm' : 'monday'}
+            </h2>
             {timeSlots
               ? timeSlots
                   .filter((timeSlot: TimeSlotType) => timeSlot.days.monday)
@@ -85,7 +98,9 @@ const ScheduleBox = ({ timeSlots }: Props) => {
                   ))}
           </div>
           <div className="relative mx-2 h-[1008px] w-1/5">
-            <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">{window.innerWidth < 535 ? 't' : 'tuesday'}</h2>
+            <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">
+              {window.innerWidth < 535 ? 't' : 'tuesday'}
+            </h2>
             {timeSlots
               ? timeSlots
                   .filter((timeSlot: TimeSlotType) => timeSlot.days.tuesday)
@@ -124,7 +139,9 @@ const ScheduleBox = ({ timeSlots }: Props) => {
                   ))}
           </div>
           <div className="relative mx-2 h-[1008px] w-1/5">
-            <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">{window.innerWidth < 535 ? 'w' : 'wednesday'}</h2>
+            <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">
+              {window.innerWidth < 535 ? 'w' : 'wednesday'}
+            </h2>
             {timeSlots
               ? timeSlots
                   .filter((timeSlot: TimeSlotType) => timeSlot.days.wednesday)
@@ -163,7 +180,9 @@ const ScheduleBox = ({ timeSlots }: Props) => {
                   ))}
           </div>
           <div className="relative mx-2 h-[1008px] w-1/5">
-            <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">{window.innerWidth < 535 ? 'th' : 'thursday'}</h2>
+            <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">
+              {window.innerWidth < 535 ? 'th' : 'thursday'}
+            </h2>
             {timeSlots
               ? timeSlots
                   .filter((timeSlot: TimeSlotType) => timeSlot.days.thursday)
@@ -202,7 +221,9 @@ const ScheduleBox = ({ timeSlots }: Props) => {
                   ))}
           </div>
           <div className="relative mx-2 h-[1008px] w-1/5">
-            <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">{window.innerWidth < 535 ? 'f' : 'friday'}</h2>
+            <h2 className="absolute -inset-8 text-center text-lg font-medium capitalize">
+              {window.innerWidth < 535 ? 'f' : 'friday'}
+            </h2>
             {timeSlots
               ? timeSlots
                   .filter((timeSlot: TimeSlotType) => timeSlot.days.friday)
