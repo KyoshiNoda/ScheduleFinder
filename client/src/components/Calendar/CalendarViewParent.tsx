b779560e307da4150b4aad84b459db0c368c854a
Carlos Duque <carlos.d.duque.r@gmail.com>
2024-06-12 10:50:00 -0400

    Saving progress.

---
+++ client/src/components/Calendar/CalendarViewParent.tsx
@@ -0,0 +1,110 @@
+import { Button } from 'flowbite-react';
+import { useState } from 'react';
+
+interface Props {}
+
+const CalendarViewParent = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      <div className="h-56 rounded-md border">
+        <header className="flex items-center justify-between border border-t-0 border-l-0 border-r-0 bg-gray-50 px-6 py-4">
+          <h1 className="text-md font-semibold">June 2024</h1>
+          <div className="relative flex items-center gap-6">
+            <Button.Group>
+              <Button color="gray">
+                <svg
+                  className="h-[16px] w-[16px] text-gray-500 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m15 19-7-7 7-7"
+                  />
+                </svg>
+              </Button>
+              <Button color="gray">
+                <svg
+                  className="h-[16px] w-[16px] text-gray-500 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m9 5 7 7-7 7"
+                  />
+                </svg>
+              </Button>
+            </Button.Group>
+
+            <button
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              className="inline-flex items-center rounded-lg p-2 text-center text-sm font-medium text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-50 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              type="button"
+            >
+              <svg
+                className="h-5 w-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 16 3"
+              >
+                <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+              </svg>
+            </button>
+
+            {isDropdownOpen && (
+              <div className="absolute top-12 z-10 w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:divide-gray-600 dark:bg-gray-700">
+                <div className="py-1">
+                  <button className="w-full px-4 py-2 text-start text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                    Separated link
+                  </button>
+                </div>
+                <div className="py-1">
+                  <button className="w-full px-4 py-2 text-start text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                    Separated link
+                  </button>
+                </div>
+                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+                  <li>
+                    <button className="w-full px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                      Dashboard
+                    </button>
+                  </li>
+                  <li>
+                    <button className="w-full px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                      Settings
+                    </button>
+                  </li>
+                  <li>
+                    <button className="w-full px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                      Earnings
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </header>
+      </div>
+    </>
+  );
+};
+
+export default CalendarViewParent;
--- client/src/pages/SchedulePage.tsx
+++ client/src/pages/SchedulePage.tsx
@@ -1,6 +1,7 @@
 import TimeSlotInput from '../components/Calendar/TimeSlotInput';
 import ScheduleBox from '../components/Calendar/ScheduleBox';
 import Toast from '../components/Globals/Toast';
+import CalendarViewParent from '../components/Calendar/CalendarViewParent';
 import { useAppDispatch } from '../redux/store';
 import { toggleReadOnly } from '../redux/feats/globalSlice/globalSlice';
 import { useAppSelector } from '../redux/store';
@@ -9,14 +10,16 @@ const SchedulePage = () => {
   const scheduleToast = useAppSelector((state: any) => state.globalSlice.toast);
   dispatch(toggleReadOnly(false));
 
-
   return (
-    <div className="flex min-h-full flex-col gap-10 bg-gray-50 py-5 px-8 dark:bg-slate-900 2xl:px-12">
-      <div className="flex flex-col lg:flex-row lg:gap-4 2xl:gap-10 mt-16">
-        <ScheduleBox timeSlots={undefined} />
-        <TimeSlotInput />
-      </div>
-      {scheduleToast.state && <Toast message={scheduleToast.message} />}
+    // <div className="flex min-h-full flex-col gap-10 bg-gray-50 py-5 px-8 dark:bg-slate-900 2xl:px-12">
+    //   <div className="mt-16 flex flex-col lg:flex-row lg:gap-4 2xl:gap-10">
+    //     <ScheduleBox timeSlots={undefined} />
+    //     <TimeSlotInput />
+    //   </div>
+    //   {scheduleToast.state && <Toast message={scheduleToast.message} />}
+    // </div>
+    <div className="min-h-full py-10 px-5 dark:bg-slate-900 dark:text-white">
+      <CalendarViewParent />
     </div>
   );
 };
