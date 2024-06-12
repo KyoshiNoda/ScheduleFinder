e2be4b74b382c35723b44455c19255d854e7c24f
Carlos Duque <carlos.d.duque.r@gmail.com>
2024-06-12 12:18:18 -0400

    Saving progress.

--- client/src/components/Calendar/CalendarViewParent.tsx
+++ client/src/components/Calendar/CalendarViewParent.tsx
@@ -1,15 +1,22 @@
 import { Button } from 'flowbite-react';
 import { useState } from 'react';
+import { CalendarViewEnum } from '../../enums';
 
 interface Props {}
 
 const CalendarViewParent = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const [selectedView, setSelectedView] = useState<CalendarViewEnum>(CalendarViewEnum.WEEK);
+
+  const handleViewSelection = (calendarView: CalendarViewEnum) => {
+    setSelectedView(calendarView);
+    setIsDropdownOpen(false);
+  };
 
   return (
     <>
-      <div className="h-56 rounded-md border">
-        <header className="flex items-center justify-between border border-t-0 border-l-0 border-r-0 bg-gray-50 px-6 py-4">
+      <div className="h-56 rounded-md border dark:border-slate-700">
+        <header className="flex items-center justify-between border border-t-0 border-l-0 border-r-0 bg-gray-50 px-6 py-4 dark:border-slate-700 dark:bg-gray-800">
           <h1 className="text-md font-semibold">June 2024</h1>
           <div className="relative flex items-center gap-6">
             <Button.Group>
@@ -55,7 +62,7 @@ const CalendarViewParent = () => {
 
             <button
               onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-              className="inline-flex items-center rounded-lg p-2 text-center text-sm font-medium text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-50 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center rounded-lg p-2 text-center text-sm font-medium text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-700 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               type="button"
             >
               <svg
@@ -70,8 +77,8 @@ const CalendarViewParent = () => {
             </button>
 
             {isDropdownOpen && (
-              <div className="absolute top-12 z-10 w-44 rounded-lg bg-white py-2 shadow dark:divide-gray-600 dark:bg-gray-700">
-                <ul className="divide-y  divide-gray-100 text-sm text-gray-700 dark:text-gray-200">
+              <div className="absolute top-12 right-0 z-10 w-36 rounded-lg bg-white py-2 shadow dark:divide-gray-600 dark:bg-gray-700">
+                <ul className="divide-y  divide-gray-100 text-sm text-gray-700 dark:text-gray-200 dark:divide-slate-600">
                   <li className="py-1">
                     <button className="w-full px-4 py-2 text-start text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
                       Create event
@@ -83,22 +90,34 @@ const CalendarViewParent = () => {
                     </button>
                   </li>
                   <li>
-                    <button className="w-full px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                    <button
+                      onClick={() => handleViewSelection(CalendarViewEnum.DAY)}
+                      className="w-full px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
                       Day view
                     </button>
                   </li>
                   <li>
-                    <button className="w-full px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                    <button
+                      onClick={() => handleViewSelection(CalendarViewEnum.WEEK)}
+                      className="w-full px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
                       Week view
                     </button>
                   </li>
                   <li>
-                    <button className="w-full px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                    <button
+                      onClick={() => handleViewSelection(CalendarViewEnum.MONTH)}
+                      className="w-full px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
                       Month view
                     </button>
                   </li>
                   <li>
-                    <button className="w-full px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                    <button
+                      onClick={() => handleViewSelection(CalendarViewEnum.YEAR)}
+                      className="w-full px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
                       Year view
                     </button>
                   </li>
@@ -107,6 +126,12 @@ const CalendarViewParent = () => {
             )}
           </div>
         </header>
+        <main>
+          {selectedView === CalendarViewEnum.DAY && <h2>Day view</h2>}
+          {selectedView === CalendarViewEnum.WEEK && <h2>Week view</h2>}
+          {selectedView === CalendarViewEnum.MONTH && <h2>Month view</h2>}
+          {selectedView === CalendarViewEnum.YEAR && <h2>Year view</h2>}
+        </main>
       </div>
     </>
   );
--- client/src/enums.ts
+++ client/src/enums.ts
@@ -1,7 +1,7 @@
 export enum Themes {
   DARK = 'black',
   LIGHT = 'white',
-  SYSTEM = 'system'
+  SYSTEM = 'system',
 }
 
 export enum TypesOfInput {
@@ -15,22 +15,29 @@ export enum ToastEnum {
   SEND_FRIEND_REQUEST = 'Friend Request Sent!',
   CANCEL_FRIEND_REQUEST = 'Canceled Friend Request!',
   REMOVED_FRIEND = 'Removed Friend!',
-  SAVED_INFO = "Saved Information!",
-  UPDATE_PROFILE_PICTURE = "Updated Profile Picture!",
-  UPDATE_EMAIL = "Updated Email!",
-  UPDATE_PASSWORD = "Updated Password!",
-  DELETE_ACCOUNT = "Deleted Account!",
-  CLEAR_SCHEDULE = "Cleared Schedule!",
-  CREATED_TIMESLOT = "Created Time Slot!",
-  UPDATE_TIMESLOT = "Updated Time Slot!",
-  DELETED_TIMESLOT = "Deleted Time Slot!",
-  ADDED_HOBBY = "Added Hobby!",
-  EDITED_HOBBY = "Edited Hobby!",
-  REMOVE_HOBBY = "Removed Hobby!"
+  SAVED_INFO = 'Saved Information!',
+  UPDATE_PROFILE_PICTURE = 'Updated Profile Picture!',
+  UPDATE_EMAIL = 'Updated Email!',
+  UPDATE_PASSWORD = 'Updated Password!',
+  DELETE_ACCOUNT = 'Deleted Account!',
+  CLEAR_SCHEDULE = 'Cleared Schedule!',
+  CREATED_TIMESLOT = 'Created Time Slot!',
+  UPDATE_TIMESLOT = 'Updated Time Slot!',
+  DELETED_TIMESLOT = 'Deleted Time Slot!',
+  ADDED_HOBBY = 'Added Hobby!',
+  EDITED_HOBBY = 'Edited Hobby!',
+  REMOVE_HOBBY = 'Removed Hobby!',
 }
+
 export enum AccountTabEnum {
-    PROFILE = 'Profile',
-    PERSONAL = 'Personal',
-    HOBBIES = 'Hobbies'
-  }
-  
+  PROFILE = 'Profile',
+  PERSONAL = 'Personal',
+  HOBBIES = 'Hobbies',
+}
+
+export enum CalendarViewEnum {
+  DAY = 'day',
+  WEEK = 'week',
+  MONTH = 'month',
+  YEAR = 'year',
+}
