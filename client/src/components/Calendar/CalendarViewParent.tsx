b2aa9aabe9e9e61c69d6a30b4b4cc6efd1c0a8db
Carlos Duque <carlos.d.duque.r@gmail.com>
2024-06-12 12:51:27 -0400

    Added variables for the SVGs

--- client/src/components/Calendar/CalendarViewParent.tsx
+++ client/src/components/Calendar/CalendarViewParent.tsx
@@ -13,6 +13,58 @@ const CalendarViewParent = () => {
     setIsDropdownOpen(false);
   };
 
+  const leftArrowSVG = (
+    <svg
+      className="h-[16px] w-[16px] text-gray-500 dark:text-white"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m15 19-7-7 7-7"
+      />
+    </svg>
+  );
+
+  const rightArrowSVG = (
+    <svg
+      className="h-[16px] w-[16px] text-gray-500 dark:text-white"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m9 5 7 7-7 7"
+      />
+    </svg>
+  );
+
+  const horizontalDotsSVG = (
+    <svg
+      className="h-5 w-5"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 16 3"
+    >
+      <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+    </svg>
+  );
+
   return (
     <>
       <div className="h-56 rounded-md border dark:border-slate-700">
@@ -20,44 +72,8 @@ const CalendarViewParent = () => {
           <h1 className="text-md font-semibold">June 2024</h1>
           <div className="relative flex items-center gap-6">
             <Button.Group>
-              <Button color="gray">
-                <svg
-                  className="h-[16px] w-[16px] text-gray-500 dark:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m15 19-7-7 7-7"
-                  />
-                </svg>
-              </Button>
-              <Button color="gray">
-                <svg
-                  className="h-[16px] w-[16px] text-gray-500 dark:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m9 5 7 7-7 7"
-                  />
-                </svg>
-              </Button>
+              <Button color="gray">{leftArrowSVG}</Button>
+              <Button color="gray">{rightArrowSVG}</Button>
             </Button.Group>
 
             <button
@@ -65,20 +81,12 @@ const CalendarViewParent = () => {
               className="inline-flex items-center rounded-lg p-2 text-center text-sm font-medium text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-700 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               type="button"
             >
-              <svg
-                className="h-5 w-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 16 3"
-              >
-                <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
-              </svg>
+              {horizontalDotsSVG}
             </button>
 
             {isDropdownOpen && (
               <div className="absolute top-12 right-0 z-10 w-36 rounded-lg bg-white py-2 shadow dark:divide-gray-600 dark:bg-gray-700">
-                <ul className="divide-y  divide-gray-100 text-sm text-gray-700 dark:text-gray-200 dark:divide-slate-600">
+                <ul className="divide-y  divide-gray-100 text-sm text-gray-700 dark:divide-slate-600 dark:text-gray-200">
                   <li className="py-1">
                     <button className="w-full px-4 py-2 text-start text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
                       Create event
