510a89cdd1d6fce43f7d71eb5ca81403d30c5909
kyoshinoda <kyoshisew@gmail.com>
2024-07-11 22:35:07 -0400

    added cr changes

--- client/src/components/Landing/Features.tsx
+++ client/src/components/Landing/Features.tsx
@@ -1,23 +1,24 @@
 import findUsers from '../../assets/findUsers.png';
 import hourlyView from '../../assets/24HourView.png';
 import AIHobbies from '../../assets/AIHobbies.png';
+
 const Features = () => {
   return (
-    <div className="h-screen">
-      <div className="flex justify-center items-center mb-4 lg:mb-12">
-        <h1 className='text-4xl lg:text-6xl font-bold dark:text-white'>Features</h1>
+    <div className="h-screen lg:px-12 mb-12 md:mb-36">
+      <div className="mb-4 flex items-center justify-center lg:mb-12">
+        <h1 className="text-4xl font-bold dark:text-white lg:text-6xl">Features</h1>
       </div>
       <div className="flex h-1/3 gap-4">
         <img
           src={findUsers}
-          className="w-1/2 rounded-xl object-cover shadow-xl border-4 border-gray-500"
+          className="w-2/5 rounded-xl border-4 border-gray-500 object-cover shadow-xl"
           alt="Schedule Comparison"
         />
-        <div className="flex w-1/2 flex-col justify-center">
-          <span className="block text-center text-lg lg:text-3xl font-bold dark:text-white">
+        <div className="flex w-3/5 flex-col justify-center">
+          <span className="block text-center text-lg font-bold dark:text-white lg:text-3xl">
             Create meaningful connections
           </span>
-          <span className="text-center text-sm lg:text-xl text-gray-500 dark:text-gray-400">
+          <span className="text-center text-sm text-gray-500 dark:text-gray-400 lg:text-xl">
             Discover new potential friends based on shared interests and availability. Filter
             through various categories such as age, major, school, gender, and more to find
             like-minded peers to study with.
@@ -26,34 +27,34 @@ const Features = () => {
       </div>
 
       <div className="flex h-1/3 gap-4">
-        <div className="flex w-1/2 flex-col justify-center">
-          <span className="block text-center text-lg lg:text-3xl font-bold dark:text-white">
-            Sync Google Calander
+        <div className="flex w-3/5 flex-col justify-center">
+          <span className="block text-center text-lg font-bold dark:text-white lg:text-3xl">
+            Sync Google Calendar
           </span>
-          <span className="text-center text-sm md:text-xl text-gray-500 dark:text-gray-400">
+          <span className="text-center text-sm text-gray-500 dark:text-gray-400 md:text-xl">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
           </span>
         </div>
         <img
           src={hourlyView}
-          className="w-1/2 rounded-xl object-cover shadow-xl border-4 border-gray-500"
-          alt="Schedule Comparison"
+          className="w-2/5 rounded-xl border-4 border-gray-500 object-cover shadow-xl"
+          alt="Google Calendar"
         />
       </div>
 
       <div className="flex h-1/3 gap-4">
         <img
           src={AIHobbies}
-          className="w-1/2 rounded-xl object-cover shadow-xl border-4 border-gray-500"
-          alt="Schedule Comparison"
+          className="w-2/5 rounded-xl border-4 border-gray-500 object-cover shadow-xl"
+          alt="Hobbies AI"
         />
-        <div className="flex w-1/2 flex-col justify-center">
-          <span className="block text-center text-lg md:text-3xl font-bold dark:text-white">
+        <div className="flex w-3/5 flex-col justify-center">
+          <span className="block text-center text-lg font-bold dark:text-white md:text-3xl">
             User spotlight with AI
           </span>
-          <span className="text-center text-sm md:text-xl text-gray-500 dark:text-gray-400">
-            AI powered, user reccomendation based off hobbies, user interests blah blah blah dolore
+          <span className="text-center text-sm text-gray-500 dark:text-gray-400 md:text-xl">
+            AI powered, user recommendation based off hobbies, user interests, blah blah blah dolore
             magna aliqua. Ut enim ad minim veniam,
           </span>
         </div>
@@ -61,4 +62,5 @@ const Features = () => {
     </div>
   );
 };
+
 export default Features;
