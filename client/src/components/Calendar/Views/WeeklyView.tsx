bf1d671aa1873c0379c183c74c781e5b68b09ef6
Carlos Duque <carlos.d.duque.r@gmail.com>
2024-09-23 23:15:24 -0400

    Removed magic numbers.

--- client/src/components/Calendar/Views/WeeklyView.tsx
+++ client/src/components/Calendar/Views/WeeklyView.tsx
@@ -3,42 +3,23 @@ import { useState } from 'react';
 import { startOfWeek, endOfWeek, addDays } from 'date-fns';
 import { generateWeekDates } from '../../../utils/scheduleUtils';
 import { format } from 'date-fns';
-import { WEEK_DAYS } from '../../../utils/constants';
+import { WEEK_DAYS, CALENDAR_HOURS } from '../../../utils/constants';
 
 type Props = {
   currentDate: Date;
 };
 
-const HOURS = [
-  '12AM',
-  '1AM',
-  '2AM',
-  '3AM',
-  '4AM',
-  '5AM',
-  '6AM',
-  '7AM',
-  '8AM',
-  '9AM',
-  '10AM',
-  '11AM',
-  '12PM',
-  '1PM',
-  '2PM',
-  '3PM',
-  '4PM',
-  '5PM',
-  '6PM',
-  '7PM',
-  '8PM',
-  '9PM',
-  '10PM',
-  '11PM',
-];
+const getNumberOfTimeBlocks = () => {
+  const hoursAWeek = 168;
+  const slotsPerHour = hoursAWeek * 2;
+  const additionalSlotRow = 7;
+
+  return slotsPerHour + additionalSlotRow;
+};
 
 const WeeklyView = ({ currentDate }: Props) => {
   const dates = generateWeekDates();
-
+  console.log('dates:', dates);
   return (
     <div className="h-[750px] w-full overflow-y-scroll pl-12 pr-7">
       <div className="sticky top-0 z-10 grid grid-cols-7 bg-white shadow">
@@ -50,7 +31,7 @@ const WeeklyView = ({ currentDate }: Props) => {
         ))}
       </div>
       <div className="grid grid-cols-7">
-        {new Array(336).fill(0).map((_, index) => (
+        {new Array(getNumberOfTimeBlocks()).fill('_').map((_, index) => (
           <div className={`border-[0.5px] ${index < 7 ? 'h-7' : 'h-14'}`}></div>
         ))}
       </div>
