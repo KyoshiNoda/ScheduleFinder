50e5ec48cf4d917a896993a15454c2a60c88d0d6
Carlos Duque <carlos.d.duque.r@gmail.com>
2024-09-30 10:27:58 -0400

    Changed the position of the top seven days so that they go all the way across.

--- client/src/components/Calendar/Views/WeeklyView.tsx
+++ client/src/components/Calendar/Views/WeeklyView.tsx
@@ -20,19 +20,12 @@ const WeeklyView = ({ initialDisplayDate }: Props) => {
   const dates = generateWeekDates(initialDisplayDate);
 
   return (
-    <div className="relative h-[750px] w-full overflow-y-scroll pl-12 pr-7">
-      <div className="absolute left-[10px] top-[74px] flex flex-col gap-y-24">
-        {CALENDAR_HOURS.map((hour) => (
-          <span key={hour} className="text-xs text-gray-400">
-            {hour}
-          </span>
-        ))}
-      </div>
-      <div className="sticky top-0 z-10 grid grid-cols-7 bg-white shadow">
+    <>
+      <div className="sticky top-0 z-10 grid grid-cols-7 border-b-[0.5px] bg-white pl-12 pr-7 shadow dark:border-gray-700 dark:bg-slate-900">
         {dates.map((date, index) => (
           <div
             key={index}
-            className="flex h-14 items-center justify-center gap-1 border-[0.5px] text-sm dark:border-gray-700 dark:bg-slate-900"
+            className="flex h-14 items-center justify-center gap-1 border-x-[0.5px] text-sm dark:border-gray-700 dark:bg-slate-900"
           >
             <span
               className={cn('text-gray-500 dark:text-gray-300', {
@@ -51,15 +44,25 @@ const WeeklyView = ({ initialDisplayDate }: Props) => {
           </div>
         ))}
       </div>
-      <div className="grid grid-cols-7">
-        {new Array(getNumberOfTimeBlocks()).fill('_').map((_, index) => (
-          <div
-            key={index}
-            className={cn('h-14 border-[0.5px] dark:border-gray-700', { 'h-7': index < 7 })}
-          ></div>
-        ))}
+
+      <div className="relative h-[750px] w-full overflow-y-scroll pl-12 pr-7">
+        <div className="absolute left-[10px] top-[74px] flex flex-col gap-y-24">
+          {CALENDAR_HOURS.map((hour) => (
+            <span key={hour} className="text-xs text-gray-400">
+              {hour}
+            </span>
+          ))}
+        </div>
+        <div className="grid grid-cols-7">
+          {new Array(getNumberOfTimeBlocks()).fill('_').map((_, index) => (
+            <div
+              key={index}
+              className={cn('h-14 border-[0.5px] dark:border-gray-700', { 'h-7': index < 7 })}
+            ></div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
