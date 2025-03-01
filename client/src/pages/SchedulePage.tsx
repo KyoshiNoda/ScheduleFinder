e8002fc09b4e97e4e11e7ad70161804813804fc1
kyoshinoda <kyoshisew@gmail.com>
2025-03-01 15:21:33 -0500

    added schedule fix

--- client/src/pages/SchedulePage.tsx
+++ client/src/pages/SchedulePage.tsx
@@ -11,15 +11,12 @@ const SchedulePage = () => {
   dispatch(toggleReadOnly(false));
 
   return (
-    // <div className="flex min-h-full flex-col gap-10 bg-gray-50 py-5 px-8 dark:bg-slate-900 2xl:px-12">
-    //   <div className="mt-16 flex flex-col lg:flex-row lg:gap-4 2xl:gap-10">
-    //     <ScheduleBox timeSlots={undefined} />
-    //     <TimeSlotInput />
-    //   </div>
-    //   {scheduleToast.state && <Toast message={scheduleToast.message} />}
-    // </div>
-    <div className="h-screen p-5 dark:bg-slate-900 dark:text-white">
-      <CalendarViewParent />
+    <div className="flex min-h-full flex-col gap-10 bg-gray-50 py-5 px-8 dark:bg-slate-900 2xl:px-12">
+      <div className="mt-16 flex flex-col lg:flex-row lg:gap-4 2xl:gap-10">
+        <ScheduleBox timeSlots={undefined} />
+        <TimeSlotInput />
+      </div>
+      {scheduleToast.state && <Toast message={scheduleToast.message} />}
     </div>
   );
 };
